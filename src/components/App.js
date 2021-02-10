ff04bf696274f5e03616abb5717de534d655b003
Logan Goulbourn <goulbournlogan@gmail.com>
2021-02-10 11:10:06 +0000

    fixing the ./createArea.js error

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Header from "./Header";
 import Footer from './Footer';
 import Note from "./Note";
-import CreateArea from "./CreateArea";
+import CreateArea from "./createArea";
 
 //this is a function for the creation of new notes
 function App() {
